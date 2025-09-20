492ef25094481d87b5c61294ba65790c40fbf993
d.romanov <d.romanov@sunstrikestudios.com>
2025-09-21 00:48:55 +0300

    Solution to Lesson 3.6 Stepik

--- Section1/test_items.py
+++ Section1/test_items.py
@@ -1,64 +1,19 @@
 import pytest
-import time, math
-from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-with open("login.txt") as file:  # чтение файла txt с двумя строками. 1я email, 2я password
-    data = file.readlines()  # файл login.txt должен находиться в папке с этим проектом
-    text = ""
+# По умолчанию запускается в хроме с русским языком, пример запуска с французским: pytest --language=fr test_items.py
 
-
-@pytest.fixture(scope="class")  # тесты так же работают со scope="function"
-def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-    print(text)  # вывод нужного текста в консоль
-
-
-class TestLoginAndAnswer:
-
-    @pytest.mark.parametrize("link", [
-        "https://stepik.org/lesson/236895/step/1",
-        "https://stepik.org/lesson/236896/step/1",
-        "https://stepik.org/lesson/236897/step/1",
-        "https://stepik.org/lesson/236898/step/1",
-        "https://stepik.org/lesson/236899/step/1",
-        "https://stepik.org/lesson/236903/step/1",
-        "https://stepik.org/lesson/236904/step/1",
-        "https://stepik.org/lesson/236905/step/1"
-    ])
-    def test_website_autorization_wait_for_modal_close(self, driver, link):
-
-        global text
-        driver.get(link)
-        driver.implicitly_wait(3)
-        wait = WebDriverWait(driver, 3)
-
-        # ниже проверка на введёную уч. запись. Если нет, то логинится.
-        check_exist = len(driver.find_elements(By.CSS_SELECTOR, ".navbar__profile"))
-        if check_exist == 0:  # проверка присутствия элемента, если check_exist == 0: отсутствует.
-            driver.find_element(By.CSS_SELECTOR, "#main-navbar .navbar__auth_login").click()
-            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".auth-widget.sign-form")))
-            driver.find_element(By.CSS_SELECTOR, "#id_login_email").send_keys(data[0])
-            driver.find_element(By.CSS_SELECTOR, "#id_login_password").send_keys(data[1])
-            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".sign-form__btn[type='submit']"))).click()
-            wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, ".auth-widget.sign-form")))
-
-        # ниже 2 строки это проверка на доступность text input. Если блок. то жмётся кнопка "решить снова".
-        disabled = driver.find_element(By.CSS_SELECTOR, ".textarea").get_attribute('disabled')
-        if disabled is not None: driver.find_element(By.CSS_SELECTOR, ".again-btn").click()
-
-        answer = math.log(int(time.time()))
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".textarea"))).send_keys(str(answer))
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".submit-submission"))).click()
-        check = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".smart-hints p"))).text
-        if check != "Correct!": text += check
-
-        assert "Correct!" == check, "Ответ не верный :)"
-
+def test_check_button(browser):
+    browser.implicitly_wait(3)
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    button_add_to_basket = browser.find_elements(By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary btn-add-to-basket"]')
+    if len(button_add_to_basket) > 0:
+        print(f'Кнопка добавления в корзину присутствует, текст на кнопке {button_add_to_basket[0].text}'
+              f'Браузер закроется через 30 секунд')
+    else:
+        assert False, 'Кнопка добавления в корзину не найдена'
+    time.sleep(30)
 
 if __name__ == "__main__":
     pytest.main()
